009e16a864b1cca88fc645f76abd55922cc1059b
Sebastian Sobociński <hiddenboox@gmail.com>
2016-04-18 23:47:06 +0200

    added banner

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,12 @@
+// Copyright (c) 2016
+//   ____        ____       ____             _
+//  / ___| ___  / ___| ___ | __ ) _   ___  _(_)
+// | |  _ / _ \| |  _ / _ \|  _ \| | | \ \/ / |
+// | |_| | (_) | |_| | (_) | |_) | |_| |>  <| |
+//  \____|\___/ \____|\___/|____/ \__,_/_/\_\_|
+//
+// Licensed under MIT.
+
 declare module 'react-isomorphic-render' {
     function webPageServer(options: {}): void;
 
